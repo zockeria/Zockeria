11683c24113997627475ef2a091702c573c0fbbd
zockeria <zockeriaglobal@gmail.com>
2026-03-23 19:00:38 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,6 +4,7 @@ import { useState } from 'react'
 import dynamic from 'next/dynamic'
 import Intro from './components/Intro'
 
+
 const Map = dynamic(() => import('./components/Map'), { ssr: false })
 
 export default function Home() {
